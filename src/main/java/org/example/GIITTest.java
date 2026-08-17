2996cac2a175325576d2c028024f1633e294309c
SowjanyaKommuri0210 <sowjanya.l.kommuri@gmail.com>
2026-08-17 10:19:14 +0530

    MyFirstCommit

---
+++ src/main/java/org/example/GIITTest.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class GIITTest {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.println("This is my first test to git from intellij ide");
+    }
+}
